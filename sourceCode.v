b9eee66741728e8bff89a5cade71819c5efa8974
0xaB26 <c2.daliabdellatif@gmail.com>
2024-12-04 10:59:38 +0100

    Update sourceCode.v

--- sourceCode.v
+++ sourceCode.v
@@ -3,14 +3,14 @@ module sourceCode(clock, reset, in, out);
 	input wire clock, reset, in;
 	output reg out;
 
-	parameter S0 = 3'b000, S1 = 3'b001, S2 = 3'b010, S3 = 3'b011, S4 = 3'b100;
+	parameter S0 = 3'b000, S1 = 3'b001, S2 = 3'b010, S3 = 3'b011, S4 = 3'b100;		// 5 states labled S0, S1, S2, S3, S4
 
 	reg[2:0] currentState;
 
 	always @(posedge clock or posedge reset) begin
-		if(reset) begin
-			currentState <= S0;
-			out <= 1'b0;
+		if(reset) begin					// Asynchrounous reset, not sync
+			currentState <= S0;			// S0 initiale state 
+			out <= 1'b0;				// output value is Zero
 		end
 		else begin
 			case(currentState) begin
